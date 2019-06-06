c82a6debb40f5129cea44076d7c92205fac13833
Ben Lye <ben@lye.co.nz>
2019-06-06 19:04:49 +0100

    Add python script to allow custom flags

--- build_jumpertx.sh
+++ build_jumpertx.sh
@@ -14,18 +14,9 @@ cp -r /jumpertx /tmp/jumpertx
 # Move to the build directory
 cd /build
 
-# Run cmake to configure the build using the standard command for the T16
-echo "Configuring..."
-cmake -DPCB=T16 -DGUI=YES -DGVARS=YES -DHELI=YES -DLCD_DUAL_BUFFER=YES -DLUA=YES -DLUA_COMPILER=YES -DMODULE_R9M_FULLSIZE=YES -DMULTIMODULE=YES -DPPM_CENTER_ADJUSTABLE=YES -DPPM_UNIT=US -DRAS=YES -DDISABLE_COMPANION=YES -DCMAKE_BUILD_TYPE=Release /tmp/jumpertx
+echo "Flags: $*"
 
-# Compile the firmware
-echo "Compiling..."
-make -j2 firmware
+python /build/build_firmware.py $*
 
-# Copy the buld back to the source path
-echo "Copying compiled firmware to source tree..."
-cp -r /build/firmware.bin /jumpertx/
-
-echo
 echo "Done."
 echo
