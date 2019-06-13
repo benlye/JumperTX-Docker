7f7a31b8bf00bf3d9a4dcf0ca2a2768c492f849f
Ben Lye <ben@lye.co.nz>
2019-06-13 11:40:28 +0100

    Update travis_tests.sh

--- travis_tests.sh
+++ travis_tests.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -e
 
-#start_fold() { echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"; }
+# Some helpers which will 'fold' the output to make the Travis logs easier to grok
 start_fold() { echo -e "travis_fold:start:$1\r"; }
 end_fold() { echo -e "\ntravis_fold:end:$1\r"; }
+run_folded_command() { start_fold $1; echo $2; $2; end_fold $1; }
 
-#run_folded_command() { start_fold $1; $2; end_fold $1; }
+echo
 # Build for T16 with no extra cmake flags
-start_fold "T16.default"
-echo "docker run --rm -it -v /tmp/JumperTX:/jumpertx benlye/jumpertx-build"
-docker run --rm -it -v /tmp/JumperTX:/jumpertx benlye/jumpertx-build
-end_fold "T16.default"
+run_folded_command "T16.default" "docker run --rm -it -v /tmp/JumperTX:/jumpertx benlye/jumpertx-build"
+
+run_folded_command "T16.lang.German" 'docker run --rm -it -v /tmp/JumperTX:/jumpertx -e "CMAKE_FLAGS=TRANSLATIONS=DE" benlye/jumpertx-build'
+
+run_folded_command "T12.default" 'docker run --rm -it -v /tmp/JumperTX:/jumpertx -e "CMAKE_FLAGS=PCB=T12" benlye/jumpertx-build'
+
+#start_fold "T16.default" "docker run --rm -it -v /tmp/JumperTX:/jumpertx benlye/jumpertx-build"
+#echo "docker run --rm -it -v /tmp/JumperTX:/jumpertx benlye/jumpertx-build"
+#docker run --rm -it -v /tmp/JumperTX:/jumpertx benlye/jumpertx-build
+#end_fold "T16.default"
