e4c02801b6ef12494dff8ff0dfe2a24f4ee424e4
Ben Lye <ben@lye.co.nz>
2019-06-13 11:15:41 +0100

    Update travis_tests.sh

--- travis_tests.sh
+++ travis_tests.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -ev
+set -e
 
 start_fold() { echo -e "travis_fold:start:$1\033[33;1m$2\033[0m"; }
 end_fold() { echo -e "\ntravis_fold:end:$1\r"; }
 
+#run_folded_command() { start_fold $1; $2; end_fold $1; }
 # Build for T16 with no extra cmake flags
 start_fold "T16.default"
+echo "docker run --rm -it -v /tmp/JumperTX:/jumpertx benlye/jumpertx-build"
 docker run --rm -it -v /tmp/JumperTX:/jumpertx benlye/jumpertx-build
 end_fold "T16.default"
